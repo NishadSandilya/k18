21e36670bb2d768d4d5368e49d9595dbe7767d3e
Nishad Sandilya <67221201+NishadSandilya@users.noreply.github.com>
2021-03-20 14:05:38 +0530

    routerupdate

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 //All imports here:
 import App from './App';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById('__te_onlineBanking'));
+ReactDOM.render(<Router basename = "/"><App/></Router>, document.getElementById('__te_onlineBanking'));
